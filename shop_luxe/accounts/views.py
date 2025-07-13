888542109f52490c718a0e8655206f64ed2c18b8
0AliPrs0 <alip13840505@gmail.com>
2025-07-13 04:02:33 +0330

    add register user api

--- shop_luxe/accounts/views.py
+++ shop_luxe/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserRegistrationSerializer, UserProfileSerializer
 
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        refresh = RefreshToken.for_user(user)
+        
+        user_data = UserProfileSerializer(user).data
+
+        return Response({
+            "user": user_data,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "message": "User registered successfully."
+        }, status=status.HTTP_201_CREATED)
+
